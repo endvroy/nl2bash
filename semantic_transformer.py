8e09c10348fc344d9437378fe7c291c009e91848
Ruoyi Wang <endvroy@gmail.com>
2019-05-10 00:02:13 +0800

    basic version mostly working

--- semantic_transformer.py
+++ semantic_transformer.py
@@ -1,6 +1,7 @@
 from bashlint import lint
 from bash_parser.bash_ast import BashAST
 from bash_parser.normalize_tokens import get_normalize_tokens
+from bash_parser import bash_loader
 
 
 def get_nast(line):
@@ -11,7 +12,10 @@ def transform_nast(nast):
     # nast node types: root, utility, flag, argument
     if not nast.children:
         # leaf node
-        return BashAST(kind=nast.kind, value=nast.value)
+        if nast.kind == 'argument':
+            return BashAST(kind=nast.arg_type, value=nast.value)
+        else:
+            return BashAST(kind=nast.kind, value=nast.value)
     else:
         children = [transform_nast(n) for n in nast.children]
         return BashAST(kind=nast.kind, value=nast.value, children=children)
@@ -19,30 +23,44 @@ def transform_nast(nast):
 
 def sem_trans_ast(ast):
     if ast.kind == 'cmd':
-        tmp_ast = BashAST(kind='cmd', prog=ast.prog, args=ast.args)
+        tmp_ast = BashAST(kind='cmd', prog=ast.prog, args=ast.args,
+                          assign_list=[], redir=[])
         # mask the args
         # todo: gather info from children in case of substitution
         name_subst_map = {}
-        subst_template = '__SUBST_{}__'
-        i = 0
-        for arg in tmp_ast.args:
+        subst_template = '#SUBST_{}#'
+        for i, arg in enumerate(tmp_ast.args):
+            # only mask arg containing subst
             for part in arg.parts:
-                # todo: only mask subst?
-                mask = subst_template.format(i)
-                name_subst_map[mask] = part
-                part[i] = mask
-                i += 1
+                if part.kind in ['cst',
+                                 'lpst',
+                                 'rpst',
+                                 'arith_subst',
+                                 'param_exp_hash',
+                                 'param_exp_repl',
+                                 'dquote_str']:
+                    mask = subst_template.format(i)
+                    name_subst_map[mask] = arg
+                    tmp_ast.args[i] = BashAST(kind='arg', parts=[mask])
+                    break
         tmp_tokens = get_normalize_tokens(tmp_ast)
         tmp_line = ''.join(tmp_tokens)
+
+        # get semantic info and transform the nast back to ast
         nast = lint.normalize_ast(tmp_line)
         masked_ast = transform_nast(nast)
-        for child in masked_ast.children:
-            if child.kind == 'argument':
-                pass
-                # todo: fill in
+        assert masked_ast.kind == 'root' and len(masked_ast.children) == 1
+        util_node = masked_ast.children[0]
+
+        for child in util_node.children:
+            pass
+
+        return util_node
 
 
 if __name__ == '__main__':
     line = input()
-    nast = get_nast(line)
-    ast = transform_nast(nast)
+    # nast = get_nast(line)
+    # ast = transform_nast(nast)
+    ast = bash_loader.parse(line)
+    sem_nast = sem_trans_ast(ast.last_cmd)
