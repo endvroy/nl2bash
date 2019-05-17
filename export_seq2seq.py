9359c616ae20f15769655af59952bd30bc2d59b9
Ruoyi Wang <endvroy@gmail.com>
2019-05-17 21:18:06 +0800

    now also exports sketch

--- export_seq2seq.py
+++ export_seq2seq.py
@@ -12,22 +12,28 @@ if __name__ == '__main__':
     for dataset in ['train', 'dev', 'test']:
         IN_PATH = IN_TMPL.format(dataset)
         OUT_PATH = OUT_TMPL.format(dataset)
+        SKETCH_PATH = SKETCH_TMPL.format(dataset)
         READBACK_PATH = READBACK_TMPL.format(dataset)
 
         with open(IN_PATH, 'rb') as in_f:
             trees = pickle.load(in_f)
         all_tokens = []
+        all_sketch = []
         for tree in trees:
-            # try:
             tokens = get_normalize_tokens(tree)
-            # except:
-            #     tokens = []
             all_tokens.append(tokens)
+            sketch = get_normalize_tokens(tree, sketch=True)
+            all_sketch.append(sketch)
 
         with open(OUT_PATH, 'w') as out_f:
             for tokens in all_tokens:
                 out_f.write(json.dumps(tokens))
                 out_f.write('\n')
+
+        with open(SKETCH_PATH, 'w') as sketch_f:
+            for sketch in all_sketch:
+                sketch_f.write(json.dumps(sketch))
+                sketch_f.write('\n')
 
         with open(READBACK_PATH, 'w') as rb_f:
             for tokens in all_tokens:
